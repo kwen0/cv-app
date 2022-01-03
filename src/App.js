f37c10137ea2bdfb112aff863b4e85c62b7b8025
Karen <karen.wen3@gmail.com>
2022-01-03 17:13:43 -0500

    finished header and contact section component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Contact from "./components/Contact";
 
-function App() {
-  return (
-    <div>
+class App extends React.Component {
+  constructor() {
+    super();
 
+    this.state = {
+      contact: {
+        name: '',
+        email: '',
+        phone: ''
+      }
+    }
+  }
+
+  handleContactChange = (e) => {
+    this.setState({
+      contact: {
+        ...this.state.contact,
+        [e.target.id]: e.target.value
+      }
+    })
+  }
+
+  handleContactSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.contact.name)
+    console.log(this.state.contact.email)
+    console.log(this.state.contact.phone)
+  }
+
+  render() {
+    return <div>
+      <Header />
+      <Contact contact={this.state.contact} handleChange={this.handleContactChange} handleSubmit={this.handleContactSubmit} />
     </div>
-  );
+  }
 }
 
 export default App;
