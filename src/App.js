67604a0989b09d1508993d533d05ecc101faddb5
Karen <karen.wen3@gmail.com>
2022-01-10 13:27:36 -0500

    updated contact and education section to use functional components

--- src/App.js
+++ src/App.js
@@ -1,161 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Contact from './components/Contact';
+import Education from './components/Education'
 import './App.css';
-import Header from "./components/Header";
-import Contact from "./components/Contact";
-import ContactSaved from './components/ContactSaved';
-import Education from './components/Education';
-import EducationSaved from './components/EducationSaved';
-import Work from './components/Work'
-import WorkSaved from './components/WorkSaved'
 
-class App extends React.Component {
-  constructor() {
-    super();
+function App() {
 
-    this.state = {
-      contact: {
-        name: '',
-        email: '',
-        phone: ''
-      },
-      editContact: true,
-      edu: {
-        id: Date.now().toString(),
-        school: '',
-        major: '',
-        years: ''
-      },
-      eduList: [],
-      editEdu: true,
-      work: {
-        company: '',
-        position: '',
-        tasks: '',
-        years: ''
-      },
-      workList: [],
-      editWork: true,
-    }
-  }
+  return (
+    <div>
+      <h2>Welcome!</h2>
+      <h3>Enter the details below in order to generate your CV.</h3>
+      <Contact />
+      <Education />
 
-  toggleEditContact = () => {
-    this.setState({ editContact: !this.state.editContact })
-  }
-
-  toggleEditEdu = () => {
-    this.setState({ editEdu: !this.state.editEdu })
-  }
-
-  toggleEditWork = () => {
-    this.setState({ editWork: !this.state.editWork })
-  }
-
-  handleContactChange = (e) => {
-    this.setState({
-      contact: {
-        ...this.state.contact,
-        [e.target.id]: e.target.value
-      }
-    })
-  }
-
-  handleEduChange = (e) => {
-    this.setState({
-      edu: {
-        ...this.state.edu,
-        [e.target.id]: e.target.value
-      }
-    })
-  }
-
-  handleWorkChange = (e) => {
-    this.setState({
-      work: {
-        ...this.state.work,
-        [e.target.id]: e.target.value
-      }
-    })
-  }
-
-  handleEduSubmit = (e) => {
-    e.preventDefault();
-    this.setState({
-      eduList: this.state.eduList.concat(this.state.edu),
-      edu: {
-        id: Date.now().toString(),
-        school: '',
-        major: '',
-        years: ''
-      },
-      editEdu: !this.state.editEdu
-    })
-  }
-
-  handleWorkSubmit = (e) => {
-    e.preventDefault();
-    this.setState({
-      workList: this.state.workList.concat(this.state.work),
-      work: {
-        company: '',
-        position: '',
-        tasks: '',
-        years: ''
-      },
-      editWork: !this.state.editWork
-    })
-  }
-
-  delEduButton = (id) => {
-    this.setState({
-      eduList: this.state.eduList.filter(edu => edu.id !== id)
-    })
-  }
-
-  delWorkButton = (company) => {
-    this.setState({
-      workList: this.state.workList.filter(work => work.company !== company)
-    })
-  }
-
-  render() {
-    const { contact, editContact, edu, eduList, editEdu, work, workList, editWork } = this.state;
-    return <div>
-      <Header />
-
-      {/* Contact Information Section */}
-      {editContact ?
-        <div>
-          <Contact contact={contact} handleChange={this.handleContactChange} />
-          <button onClick={this.toggleEditContact}>Save</button>
-        </div> :
-        <div>
-          <ContactSaved contact={contact} />
-          <button onClick={this.toggleEditContact}>Edit</button>
-        </div>
-      }
-
-      {/* Education Section */}
-      <div className="section-title">Education</div>
-      <EducationSaved eduList={eduList} delButton={this.delEduButton} />
-      {editEdu ?
-        <Education edu={edu} handleChange={this.handleEduChange} handleSubmit={this.handleEduSubmit} />
-        :
-        <button onClick={this.toggleEditEdu}>Add</button>
-      }
-
-      {/* Professional Experience Section */}
-      <div className="section-title">Professional Experience</div>
-      <WorkSaved workList={workList} delButton={this.delWorkButton} />
-      {editWork ?
-        <div>
-          <Work work={work} handleChange={this.handleWorkChange} handleSubmit={this.handleWorkSubmit} />
-        </div>
-        :
-        <button onClick={this.toggleEditWork}>Add</button>
-      }
-
-    </div >
-  }
+    </div>
+  )
 }
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,32 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-class Contact extends React.Component {
-    render() {
-        return <div>
-            <div className="section-title">Contact Information</div>
-            Name:
-            <input
-                type="text"
-                onChange={this.props.handleChange}
-                value={this.props.contact.name}
-                id="name" />
-            <br></br>
-            Email:
-            <input
-                type="email"
-                onChange={this.props.handleChange}
-                value={this.props.contact.email}
-                id="email" />
-            <br></br>
-            Phone number:
-            <input
-                type="tel"
-                onChange={this.props.handleChange}
-                value={this.props.contact.phone}
-                id="phone" />
-            <br></br>
-        </div >
+function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [editMode, setEditMode] = useState(true)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setEditMode(false)
     }
-}
 
+    return (
+        <div>
+            <p className="section-title">Contact Information</p>
+            {editMode ? <div>
+                <form onSubmit={handleSubmit}>
+
+                    <label>Name:
+                        <input type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </label><br></br>
+                    <label>Email:
+                        <input type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </label><br></br>
+                    <label>Phone:
+                        <input type="text"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
+                    </label><br></br>
+                    <button>Save</button>
+                </form>
+            </div> :
+                <div>
+                    Name: {name} <br></br>
+                    Email: {email}<br></br>
+                    Phone: {phone}<br></br>
+                    <button onClick={() => setEditMode(true)}>Edit</button>
+                </div>}
+        </div>
+    )
+}
 export default Contact;
